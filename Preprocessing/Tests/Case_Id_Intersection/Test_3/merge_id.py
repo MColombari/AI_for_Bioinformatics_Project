a66ad566bca021ce70126ee1f833fbbf54b6ee0e
M Colombari <64273373+MColombari@users.noreply.github.com>
2024-12-17 16:02:12 +0100

    Push raw data

--- Preprocessing/Tests/Case_Id_Intersection/Test_3/merge_id.py
+++ Preprocessing/Tests/Case_Id_Intersection/Test_3/merge_id.py
@@ -3,25 +3,27 @@ import json
 file_names = ['CopyNumberWithoutSolidTissue.json', 'GeneExpressionWithoutSolidTissue.json', 'Methylation.json', 'OverallSurvival.json']
 # file_names = ['CopyNumber.json', 'Methylation.json', 'OverallSurvival.json']
 # file_names = ['CopyNumber.json']
-case_id_list_list = []
 
+case_id_list = []
 
 for file_name in file_names:
-    case_id_list = []
     with open(file_name, 'r') as file:
         file_parsed = json.load(file)
         for case in file_parsed:
             case_id = case["cases"][0]["case_id"]
             if not case_id in case_id_list:
-                case_id_list.append(case_id)
+                case_id_list.append(case)
     print(f"{file_name}: {len(case_id_list)}")
-    case_id_list_list.append(case_id_list)
 
+#Check for repetition.
+repetition_name = []
+for case in case_id_list:
+    case_id = case["cases"][0]["case_id"]
+    if case
 
-out = list(set(case_id_list_list[0]) & set(case_id_list_list[1]) & set(case_id_list_list[2]))
 # print(out)
 print(f"Total intersection: {len(out)}")
 
-# Save data
-with open('white_list.json', 'w', encoding='utf-8') as f:
+# Save extended data.
+with open('extended_white_list.json', 'w', encoding='utf-8') as f:
     json.dump(out, f, ensure_ascii=False, indent=4)
