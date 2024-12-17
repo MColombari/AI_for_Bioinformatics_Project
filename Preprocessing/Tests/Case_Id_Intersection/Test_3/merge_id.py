394882da87e28218a1ce3e0719e1dd6b6396d2bd
M Colombari <64273373+MColombari@users.noreply.github.com>
2024-12-17 17:46:00 +0100

    Hist test

--- Preprocessing/Tests/Case_Id_Intersection/Test_3/merge_id.py
+++ Preprocessing/Tests/Case_Id_Intersection/Test_3/merge_id.py
@@ -3,27 +3,36 @@ import json
 file_names = ['CopyNumberWithoutSolidTissue.json', 'GeneExpressionWithoutSolidTissue.json', 'Methylation.json', 'OverallSurvival.json']
 # file_names = ['CopyNumber.json', 'Methylation.json', 'OverallSurvival.json']
 # file_names = ['CopyNumber.json']
-
-case_id_list = []
+case_id_list_list = []
 
 for file_name in file_names:
+    case_id_list = []
     with open(file_name, 'r') as file:
         file_parsed = json.load(file)
         for case in file_parsed:
             case_id = case["cases"][0]["case_id"]
             if not case_id in case_id_list:
-                case_id_list.append(case)
+                case_id_list.append(case_id)
     print(f"{file_name}: {len(case_id_list)}")
+    case_id_list_list.append(case_id_list)
 
-#Check for repetition.
-repetition_name = []
-for case in case_id_list:
-    case_id = case["cases"][0]["case_id"]
-    if case
+
+out = list(set(case_id_list_list[0]) & set(case_id_list_list[1]) & set(case_id_list_list[2]))
+out_dict = []
+
+
+with open("OverallSurvival.json", 'r') as file:
+    file_parsed = json.load(file)
+    for case in file_parsed:
+        case_id = case["cases"][0]["case_id"]
+        if case_id in out:
+            out_dict.append(case)
+
 
 # print(out)
 print(f"Total intersection: {len(out)}")
+print(f"Total case written: {len(out_dict)}")
 
-# Save extended data.
+# Save data
 with open('extended_white_list.json', 'w', encoding='utf-8') as f:
-    json.dump(out, f, ensure_ascii=False, indent=4)
+    json.dump(out_dict, f, ensure_ascii=False, indent=4)
