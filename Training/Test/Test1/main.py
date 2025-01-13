91729c0aa04ce23b0917d5e26ca16a496fff9c59
MColombari <mattia.colombari00@gmail.com>
2025-01-13 16:03:18 +0100

    tmp commit

--- Training/Test/Test1/main.py
+++ Training/Test/Test1/main.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import os
 import pandas as pd
 
-def load_data(path_white_list, path_gene_expression_json, path_data):
+def load_data(path_white_list, path_gene_expression_json, path_data, path_relation):
     # Load case ID from white list
     white_list = []
     with open(path_white_list, 'r') as file:
@@ -14,15 +14,23 @@ def load_data(path_white_list, path_gene_expression_json, path_data):
             white_list.append(case_id)
 
     # Load file name relative to case id in json file in gene expression.
+    dict_file_name_to_case_id_relation = {}
     file_names = []
     with open(path_gene_expression_json, 'r') as file:
         file_parsed = json.load(file)
         for case in file_parsed:
             case_id = case["cases"][0]["case_id"]
             if case_id in white_list:
+                dict_file_name_to_case_id_relation[case['file_name']] = case_id
                 file_names.append(case["file_name"])
 
+
+    with open(path_relation, 'r') as file:
+        dict_relation = json.load(file)
+
     count = 0
+    list_list_featurevectors = []
+    list_value = []
     # Now explore data path to get the right files
     for root, dirs, files in os.walk(path_data):
         for dir in dirs:
@@ -30,15 +38,25 @@ def load_data(path_white_list, path_gene_expression_json, path_data):
                 for file in files:
                     if file in file_names:
                         # Parse file into graph
-                        parsed_file = pd.read_csv(path_data + "/" + dir + "/" + file, sep='\t')
-                        parsed_file['tpm_unstranded']
-
-                        count = count + 1
-                        print(file_names)
+                        parsed_file = pd.read_csv(
+                            path_data + "/" + dir + "/" + file, sep='\t', comment="#", skiprows=lambda x: x in [2, 3, 4, 5])
+                        
+                        case_id = dict_file_name_to_case_id_relation[file]
+                        if dict_relation[case_id]['type'] == 'dead':
+                            list_value.append(dict_relation[dict_file_name_to_case_id_relation[file]]['value'])
+                            list_list_featurevectors.append(list(parsed_file['tpm_unstranded']))
+                            count = count + 1
+                            print(count)
+                        # print(file_names)
+                        
+    print(len(list_value))
+    print(len(list_list_featurevectors))
+    print(count)
     
 
 
 
 load_data("/homes/mcolombari/AI_for_Bioinformatics_Project/Preprocessing/Tests/Case_Id_Intersection/Test_3/white_list.json",
           "/homes/mcolombari/AI_for_Bioinformatics_Project/Preprocessing/Tests/Case_Id_Intersection/Test_3/GeneExpressionWithoutSolidTissue.json",
-          "/work/h2020deciderficarra_shared/TCGA/OV/project_n16_data/GeneExpression")
+          "/work/h2020deciderficarra_shared/TCGA/OV/project_n16_data/GeneExpression",
+          "/homes/mcolombari/AI_for_Bioinformatics_Project/Preprocessing/Final/case_id_to_value_relation.json")
--- Training/Test/Test1/read_file_test.py
+++ Training/Test/Test1/read_file_test.py
@@ -7,11 +7,13 @@ import pandas as pd
 
 PATH = "/work/h2020deciderficarra_shared/TCGA/OV/project_n16_data/GeneExpression/0b52237e-f467-420a-b7b2-952c0004efc9/d90d2b4f-8865-440d-a8b7-e0b7c947e095.rna_seq.augmented_star_gene_counts.tsv"
 
-parsed_file = pd.read_csv(PATH, sep='\t')
+parsed_file = pd.read_csv(PATH, sep='\t', header=0, skiprows=lambda x: x in [0, 2, 3, 4, 5])
 print(type(parsed_file))
 print(parsed_file.dtypes)
 print(parsed_file.size)
 print(parsed_file.shape)
+print(parsed_file.columns)
 print(parsed_file)
+print(list(parsed_file["gene_id"]))
 # print(parsed_file.loc[1:])
 # print(parsed_file[4,:]['tpm_unstranded'])
