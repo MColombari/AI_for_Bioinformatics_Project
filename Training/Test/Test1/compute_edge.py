aa03af4d7fc2f62aaac9487b54da86f80d9455c7
fmanc23 <f.mancinelli23@gmail.com>
2025-01-13 16:31:39 +0100

    edges

--- Training/Test/Test1/compute_edge.py
+++ Training/Test/Test1/compute_edge.py
@@ -1,5 +1,22 @@
-
+import torch
 # list_list[[], []]
-list = [1,2,3,42,2,3,4,3,3,45,666,4,4,33]
-
+in_list = [1,2,3,42,2,3,4,3,3,45,666,4,4,33]
 t = 5
+x = list(zip(range(len(in_list)), in_list))
+
+x = sorted(x, key=lambda x: x[1])
+combinations = [[],[]]
+print(x)
+
+for i in range(len(x)):
+    for j in range(i+1, len(x)):
+        if(x[j][1] < x[i][1] + 3):
+            combinations[0].append(x[i][0])
+            combinations[0].append(x[j][0])
+            combinations[1].append(x[j][0])
+            combinations[1].append(x[i][0])
+        else:
+            break
+print(combinations[0])
+print(combinations[1])
+edge_index = torch.tensor(combinations, dtype=torch.long)
