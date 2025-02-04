ac53ae3c24f7be5462b4497258c11843277fdd50
MColombari <mattia.colombari00@gmail.com>
2025-02-04 19:31:36 +0100

    Possible final template

--- Training/Model_Template/Save_model.py
+++ Training/Model_Template/Save_model.py
@@ -1,10 +1,12 @@
 import os
 import re
 from datetime import datetime
+import torch
 
 class SaveModel:
     def __init__(self, main_folder_path, test_name):
         self.main_folder_path = main_folder_path
+        self.test_name = test_name
 
         if not os.path.isdir(main_folder_path):
             raise Exception(f"No folder with path '{main_folder_path}'")
@@ -43,3 +45,14 @@ class SaveModel:
         with open(f"{self.current_folder}/model_architecture.txt", "w") as f:
             f.write("\tModel Structure")
             f.write(str(model))
+
+    def save_epoch(self, epoch, model):
+        torch.save({
+            'epoch': epoch,
+            'model_dict':model.state_dict()}, 
+            f"{self.current_folder}/model_checkpoints/{self.test_name}_epoch_{epoch}.pth")
+
+    # logger.info("epoch {:3d} | iteration {:5d} | Loss {:.6f} | Acc {:.2f}%".format(epoch+1, batch_idx+1, loss.item(), score*100))
+    def save_epoch_data(self, epoch, loss_train, accuracy_train, loss_test, accuracy_test):
+        with open(f"{self.current_folder}/epoch_data.txt", "a") as f:
+            f.write(f"Epoch: '{epoch}', Loss Train: '{loss_train}', Accuracy Train: '{accuracy_train}, Loss Test: '{loss_test}', Accuracy Test: '{accuracy_test}'")
