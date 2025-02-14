86823197e56a29790f7adfb81f3bfd74e60930ff
MColombari <mattia.colombari00@gmail.com>
2025-02-14 18:06:00 +0100

    Convertion ENSPto ENSG

--- Training/Gene/run_files/run_train_gene.sh
+++ Training/Gene/run_files/run_train_gene.sh
@@ -4,7 +4,8 @@
 #SBATCH --job-name=gene_training
 #SBATCH --partition=all_usr_prod
 #SBATCH --gres=gpu:1
-#SBATCH --time=06:00:00
+#SBATCH --time=24:00:00
+#SBATCH --mem-per-cpu=16G
 
 #SBATCH --output="run_output/output_train.log"
 #SBATCH --error="run_output/error_train.log"
