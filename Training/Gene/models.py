c82af6f5cc20a6bcd395974cd39edf17e27f041b
MColombari <mattia.colombari00@gmail.com>
2025-02-17 18:04:18 +0100

    New model

--- Training/Gene/models.py
+++ Training/Gene/models.py
@@ -17,6 +17,11 @@ class simple_GCN(torch.nn.Module):
         self.lin = Linear(hidden_channels, num_classes)
 
     def forward(self, x, edge_index, batch=None):
+        num_nodes = x.shape[0]  # Numero di nodi
+        print(x.shape)
+        max_index = edge_index.max().item()
+        print(f"Numero di nodi: {num_nodes}, Max edge_index: {max_index}")
+        assert max_index < num_nodes, "Errore: edge_index contiene un indice fuori dai limiti!"
         # 1. Obtain node embeddings 
         x = self.conv1(x, edge_index)
         x = x.relu()
@@ -29,6 +34,35 @@ class simple_GCN(torch.nn.Module):
 
         # 3. Apply a final classifier
         x = F.dropout(x, p=0.5, training=self.training)
+        x = self.lin(x)
+        
+        return x 
+    
+
+class small_GCN(torch.nn.Module):
+    def __init__(self, input_feature, hidden_channels, num_classes):
+        # input feature comes from dataset.num_node_feature
+        # which returns the number of features per node in the dataset.
+        # So input_feature need to be number of feature per node in the dataset.
+        # source: https://pytorch-geometric.readthedocs.io/en/latest/generated/torch_geometric.data.Dataset.html
+        super(small_GCN, self).__init__()
+        self.conv1 = GCNConv(input_feature, hidden_channels)
+        self.lin = Linear(hidden_channels, num_classes)
+
+    def forward(self, x, edge_index, batch=None):
+        # num_nodes = x.shape[0]  # Numero di nodi
+        # print(x.shape)
+        # max_index = edge_index.max().item()
+        # print(f"Numero di nodi: {num_nodes}, Max edge_index: {max_index}")
+        # assert max_index < num_nodes, "Errore: edge_index contiene un indice fuori dai limiti!"
+        # 1. Obtain node embeddings 
+        x = self.conv1(x, edge_index)
+
+        # 2. Readout layer
+        x = global_mean_pool(x, batch)  # [batch_size, hidden_channels]
+
+        # 3. Apply a final classifier
+        # x = F.dropout(x, p=0.5, training=self.training)
         x = self.lin(x)
         
         return x 
